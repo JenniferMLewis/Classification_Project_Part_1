67b85c0c247d194ab2dadacc1b8a7eb72daacf2e
JenniferMLewis <lewis.jennifer.marie.15@gmail.com>
2022-08-20 22:59:44 -0500

    Altering Acquire and Prepare definitions to fit for only the Telcom data, as that's all we plan to work with.

--- acquire.py
+++ acquire.py
@@ -1,3 +1,4 @@
+import numpy as np
 import os
 import pandas as pd
 from env import user, password, host
@@ -5,46 +6,16 @@ from env import user, password, host
 def get_url(db, user=user, password=password, host=host):
     '''
     take database name for input,
-    returns url, using user, password, and host pulled from your env file.
+    returns url, using user, password, and host pulled from your .env file.
     PLEASE save it as a variable, and do NOT just print your credientials to your document.
     '''
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
-def get_titanic_data():
-    '''
-    Returns the titanic dataset, checks local disk for titanic.csv, if present loads it, 
-    otherwise it pulls the data from SQL and then saves it to local disk as 'titanic.csv'
-    (This is set up for Codeup's SQL server, with database title 'titanic_db'.)
-    '''
-    filename = "titanic.csv"
-
-    if os.path.isfile(filename):
-        return pd.read_csv(filename)
-    else:
-        df = pd.read_sql('SELECT * FROM passengers', get_url('titanic_db'))
-        df.to_csv(filename)
-        return df 
-
-def get_iris_data():
-    '''
-    Returns the iris dataset, checks local disk for iris.csv, if present loads it, 
-    otherwise it pulls the data from SQL, joins species and measurements tables and then saves it to local disk as 'iris.csv'
-    (This is set up for Codeup's SQL server, with database title 'iris_db'.)
-    '''
-    filename = "iris.csv"
-
-    if os.path.isfile(filename):
-        return pd.read_csv(filename)
-    else:
-        df = pd.read_sql('''SELECT * FROM species
-        LEFT JOIN measurements
-        USING(species_id);''', get_url('iris_db'))
-        df.to_csv(filename)
-        return df 
+# Pulling Telco Data
 
 def get_telco_data():
     '''
-    Returns the telco dataset, checks local dist for telco.csv, if present loads it,
+    Returns the telco dataset, checks local disk for telco.csv, if present loads it,
     otherwise it pulls the data from SQL, joins customer, contract_types, payment_types, internet_service_types, and customer_churn tables,
     the customer_churn has date of customer churn, as not all customers have churned, there WILL be Null Values that require tidying up, Please be aware.
     '''
@@ -64,3 +35,34 @@ def get_telco_data():
         USING(customer_id);''', get_url('telco_churn'))
         df.to_csv(filename)
         return df
+
+# Sending column names to lists depending upon their data type (number, categorical)
+
+def dtypes_to_list(df):
+    '''
+    Takes in a dataframe, returns two lists, 
+    one of num type column names, and one of categorical type column names.
+    '''
+    num_type_list, cat_type_list = [], []
+    for column in df:
+        col_type =  df[column].dtype
+        if col_type == "object" :
+            cat_type_list.append(column)
+        if np.issubdtype(df[column], np.number) and \
+            ((df[column].max() + 1) / df[column].nunique())  == 1 :
+            cat_type_list.append(column)
+        if np.issubdtype(df[column], np.number) and \
+            ((df[column].max() + 1) / df[column].nunique()) != 1 :
+            num_type_list.append(column)
+    return num_type_list, cat_type_list
+
+# range of vaues in a column
+
+def col_range(df):
+    '''
+    Takes in a data frame, returns the 'describe' of the data frame with a new entry 'range'.
+    'Range' is the difference between the 'max' and 'min' columns.
+    '''
+    stats_df = df.describe().T
+    stats_df['range'] = stats_df['max'] - stats_df['min']
+    return stats_df
--- prepare.py
+++ prepare.py
@@ -1,47 +1,10 @@
-# Running Notes:
-
-# df.select_dtypes(include='number').columns
-#    Pulls up all column names with int or float values. Nifty!
-
-# pd.concat([df, dummy_df], axis=1)
-#    Adds the data frames next to each other instead of creating new rows after the first dataframe's rows (and giving a bunch of null/nan)
-
 import numpy as np
 import pandas as pd
 import seaborn as sns
 from pydataset import data
 from sklearn.model_selection import train_test_split
 
-def prep_iris_data(df):
-    '''
-    For the Iris DB:
-    Drops the Foreign Key Columns, renames 'species_name' to species, 
-    then breaks the species into dummy columns with int values.
-    returns the Data Frame cleaned up
-    '''
-    df = df.drop(columns=['species_id', 'measurement_id', 'Unnamed: 0']).rename(columns={'species_name' : 'species'})
-    dummy_df = pd.get_dummies(df.species, drop_first=True)
-    df = pd.concat([df,dummy_df], axis=1)
-    return df
 
-
-def prep_titanic(df):
-    '''
-    For the Titanic DB:
-    Drops the pasenger_id, embarked, class, age, and deck Columns, renames 'embark_town' to 'embarked' and 'pclass' to 'passenger_class', 
-    Renames the index to passenger_id, since they are the same values, we don't need duplicates.
-    Combines 'sibsp' and 'parch' into the column 'family' then dropos 'sibsp' and 'parch'
-    then breaks 'sex' and 'embarked' into dummy columns with int values.
-    Returns the Data Frame cleaned up
-    '''
-    df['family'] = df.sibsp + df.parch
-    df = df.drop(columns=["parch", "sibsp"])
-    dummy_df = pd.get_dummies(data=df[['sex','embark_town']], drop_first=True)
-    df = pd.concat([df, dummy_df], axis=1)
-    df = df.drop(columns=['embarked', 'sex', 'embark_town','class', 'age','deck', 'Unnamed: 0', 'passenger_id'])
-    df.index.names=["passenger_id"]
-
-    return df
 
 
 def prep_telco(df):
@@ -76,31 +39,14 @@ def prep_telco(df):
 
     return df
 
+# Splits data.
+
 def data_split(df, target):
     '''
-    take in a DataFrame and target.
-    return train, validate, test DataFrames.
+    take in a DataFrame and target column; returns train, validate, test DataFrames.
+    Random State is set to the most generic '123'.
     '''
     train, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[target])
     train, validate = train_test_split(train, test_size=.25, random_state=123, stratify=train[target])
     
     return train, validate, test
-
-def prep_titanic_age(df):
-    '''
-    For the Titanic DB:
-    Drops the pasenger_id, embarked, class, and deck Columns, renames 'embark_town' to 'embarked' and 'pclass' to 'passenger_class', 
-    Renames the index to passenger_id, since they are the same values, we don't need duplicates.
-    Combines 'sibsp' and 'parch' into the column 'family' then dropos 'sibsp' and 'parch'
-    then breaks 'sex' and 'embarked' into dummy columns with int values. Lastly, fills in all NaN in 'age' with the rounded (because all ages are xx.0, may remove round() dependant upon how it affects data) mean of known ages.
-    Returns the Data Frame cleaned up
-    '''
-    df['family'] = df.sibsp + df.parch
-    df = df.drop(columns=["parch", "sibsp"])
-    dummy_df = pd.get_dummies(data=df[['sex','embark_town']], drop_first=True)
-    df = pd.concat([df, dummy_df], axis=1)
-    df = df.drop(columns=['embarked', 'sex', 'embark_town','class','deck', 'Unnamed: 0', 'passenger_id'])
-    df.index.names=["passenger_id"]
-    df.age.fillna(round(df.age.mean()), inplace=True) # rounded since all ages are xx.0
-
-    return df
